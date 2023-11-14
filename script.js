6eb3a4f9dd5de50ce7012585ced2fe2ad5acb78b
leoshliak <levshlyak@gmail.com>
2023-11-14 13:03:41 +0200

    final result

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const password = document.querySelector('#password');
-const confirmPassword = document.querySelector('#password2');
+
+function checkPasswords(){
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('password2');
+
+    if(password.value !== confirmPassword.value || confirmPassword.value == ''){
+     confirmPassword.setCustomValidity('Passwords Must Match');
+        confirmPassword.reportValidity();
+        confirmPassword.style.border = 'none';
+        confirmPassword.style.outline = '2px solid red';
+        confirmPassword.style.boxShadow = '2px 1px 6px red;';
+
+    }else{
+        confirmPassword.setCustomValidity('');
+        confirmPassword.style.border = 'none';
+        confirmPassword.style.outline = '2px solid rgb(3, 182, 3)';
+        confirmPassword.style.boxShadow = '2px 1px 6px rgb(3, 182, 3);';
+    }
+}
+
